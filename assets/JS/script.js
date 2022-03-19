dbe80ac9c03bde332357c4b2d992fe4f592833ac
Natalya Colburn <greatqueenbee@gmail.com>
2022-03-18 20:42:53 -0600

    added the reappear button click for rules and quiz box

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,7 +2,18 @@ var start_btn = document.querySelector(".start_btn button");
 var info_box = document.querySelector(".info_box");
 var exit_btn = document.querySelector(".buttons .quit");
 var continue_btn = document.querySelector(".buttons .restart");
+var quiz_box = document.querySelector(".quiz_box")
 
-start_btn.onclick = ()=>{
-    info_box.classList.add("activeInfo")
+start_btn.onclick = () => {
+    info_box.classList.add("activeInfo");
+}
+
+exit_btn.onclick = () => {
+    info_box.classList.remove("activeInfo");
+}
+
+continue_btn.onclick = () => {
+
+    info_box.classList.remove("activeInfo");
+    quiz_box.classList.add("activeQuiz");
 }
