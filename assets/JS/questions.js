ecc9c10d01db8794abcdc86bfaefdf5e89f4775c
Natalya Colburn <greatqueenbee@gmail.com>
2022-03-18 20:40:03 -0600

    did a separate file for quiz questions

---
+++ assets/JS/questions.js
@@ -0,0 +1,63 @@
+let questions = [
+{
+
+numb: 1,
+question: "Koumpounophobia is the fear of:",
+answer: "Buttons (!)",
+options: [
+    "Mountains",
+    "Rabbits",
+    "Buttons (!)",
+    "Pandas",
+]
+},
+
+{
+numb: 2,
+question: "Scolionophobia is the fear of:",
+answer: "School (!)",
+options: [
+    "Mountains",
+    "Rabbits",
+    "School (!)",
+    "Pandas",
+]
+},
+
+{
+numb: 3,
+question: "Arithmophobia is the fear of:",
+answer: "Numbers (!)",
+options: [
+    "Mountains",
+    "Rabbits",
+    "Numbers (!)",
+    "Pandas",
+]
+},
+{
+numb: 4,
+question: "Atelophobia is the fear of:",
+answer: "Imperfection (!)",
+options: [
+    "Mountains",
+    "Rabbits",
+    "Imperfection (!)",
+    "Pandas",
+]
+},
+{
+numb: 5,
+question: "Dystychiphobia is the fear of:",
+answer: "Accidents (!)",
+options: [
+    "Mountains",
+    "Rabbits",
+    "Accidents (!)",
+    "Pandas",
+]
+
+},
+
+
+];
