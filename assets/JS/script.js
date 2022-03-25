46e600ff600b1b16c39839d5eef1d563193d6c60
Natalya Colburn <greatqueenbee@gmail.com>
2022-03-25 12:16:09 -0600

    changes with tutor P Lim

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -4,12 +4,20 @@ var exit_btn = document.querySelector(".buttons .quit");
 var continue_btn = document.querySelector(".buttons .restart");
 var quiz_box = document.querySelector(".quiz_box")
 var timeCount = quiz_box.querySelector(".timer_sec")
-var que_count = 0;
+var results_box = document.querySelector(".results_box");
+var restart_quiz = results_box.querySelector(".buttons .restart");
+var quit_quiz = results_box.querySelector(".buttons .quit")
+var initalSelector =document.querySelector (".initialSection");
+var initalInput = document.querySelector ("#inital-input");
+var savebtn = document.querySelector ("#savebtn");
+
+var questionSelector = document.querySelector ("#question-section")
+var answerNum = document.querySelector ("#right-answer")
 var que_numb = 1;
 var counter;
 var timeValue = 75;
 var userScore = 0;
-
+var index = 0;
 
 
 
@@ -24,7 +32,8 @@ exit_btn.onclick = () => {
     info_box.classList.remove("activeInfo");
 }
 
-function showQuestions(index) {
+function showQuestions() {
+
     var que_text = document.querySelector(".que_text");
     var option_list = document.querySelector(".option_list");
     var que_tag = '<span>' + questions[index].numb + "." + questions[index].question + '<span>';
@@ -33,10 +42,36 @@ function showQuestions(index) {
         '<div class="option">' + questions[index].options[2] + '<span></span></div>' +
         '<div class="option">' + questions[index].options[3] + '<span></span></div>';
     que_text.innerHTML = que_tag;
+    
     option_list.innerHTML = options_tag;
-    const option = option_list.querySelectorAll(".option");
-    for (let i = 0; index < option_list.length; i++) {
-        option[i].addEventListener("click", "optionSelected(option [i])");
+    var option =document.querySelectorAll(".option");
+    for (let i = 0; i < option.length; i++) {
+        option[i].addEventListener("click",  function(){
+            let userAns= this.textContent
+           
+            if (userAns ===  questions[index].answer) {
+                userScore += 1;
+                console.log(userScore);
+               // answer.classlist.add("correct");
+               answerNum.textContent = userScore;
+                console.log("answer is correct");
+            } else {
+               // answer.classList.add("Incorrect")
+                console.log("Answer is Incorrect")
+                timeValue = timeValue -10;
+            }
+
+            index++
+            if(index <questions.length){
+                showQuestions()
+            }
+            else{
+                clearInterval(counter);
+                initalSelector.classList.remove("hide")
+                questionSelector.classList.add("hide")
+                //showResultsBox()
+            }
+        });
     }
 
 }
@@ -50,13 +85,13 @@ function questCounter(index) {
 }
 
 
-function startTimer(time) {
+function startTimer() {
     counter = setInterval(timer, 1000);
 
     function timer() {
-        timeCount.textContent = time;
-        time--;
-        if (time < 0) {
+        timeCount.textContent = timeValue;
+        timeValue--;
+        if (timeValue < 0) {
             clearInterval(counter);
             timeCount.textContent = "END";
         }
@@ -72,16 +107,14 @@ continue_btn.onclick = () => {
 
     //showQuestions(0);
     //questCounter(1);
-    startTimer(75);
-    showQuestions(0);
+    startTimer();
+    showQuestions();
     questCounter(que_numb);
     //startTimer(timeValue);
 }
 
 
-var results_box = document.querySelector(".results_box");
-var restart_quiz = results_box.querySelector(".buttons .restart");
-var quit_quiz = results_box.querySelector(".buttons .quit")
+
 
 restart_quiz.onclick = () => {
     results_box.classList.remove("activeResult");
@@ -90,7 +123,7 @@ restart_quiz.onclick = () => {
     //let que_numb = 1;
     //let timeValue = 75;
     //let userScore = 0;
-    showQuestions(que_count);
+    showQuestions();
     questCounter(que_numb);
     //startTimer(timeValue);
 
@@ -115,7 +148,7 @@ function optionSelected(answer) {
 
 
     for (let i = 0; i < select_button.length; i++) {
-        select_button[i].addEventListener("click", () => {
+        select_button.addEventListener("click", () => {
 
 
         })
@@ -126,10 +159,10 @@ function optionSelected(answer) {
     if (userAns === correctAns) {
         userScore += 1;
         console.log(userScore);
-        answer.classlist.add("correct");
+       // answer.classlist.add("correct");
         console.log("answer is correct");
     } else {
-        answer.classList.add("Incorrect")
+       // answer.classList.add("Incorrect")
         console.log("Answer is Incorrect")
         timeCount = -10;
     }
@@ -141,7 +174,7 @@ function optionSelected(answer) {
     if (que_count < questions.length - 1) {
         que_count++;
         que_numb++;
-        //showQuestions(que_count);
+        showQuestions();
         questCounter(que_numb);
         //startTimer(timeCount);
     } else {
@@ -150,25 +183,26 @@ function optionSelected(answer) {
     }
 
 
-    function showResultsBox() {
-        info_box.classList.remove("activeInfo");
-        quiz_box.classList.remove("activeQuiz");
-        results_box.classList.add("activeResult");
-        var scoreText = results_box.querySelector(".score_text");
-
-        if (userScore > 3) {
-            var scoreTag = '<span> EXCELLENT! You got <p>' + userScore + '</p> out of <p>' + questions.length + '</p></span>';
-            scoreText.innerHTML = scoreTag;
-        } else if (userScore > 1) {
-            var scoreTag = '<span> Nice...You got <p>' + userScore + '</p> out of <p>' + questions.length + '</p></span>';
-            scoreText.innerHTML = scoreTag;
-        } else {
-            var scoreTag = '<span> BummerYou got <p>' + userScore + '</p> out of <p>' + questions.length + '</p></span>';
-            scoreText.innerHTML = scoreTag;
-        }
-    }
-
-
+   
     showResultsBox();
 
 }
+
+function showResultsBox() {
+    info_box.classList.remove("activeInfo");
+    quiz_box.classList.remove("activeQuiz");
+    results_box.classList.add("activeResult");
+    var scoreText = results_box.querySelector(".score_text");
+
+    if (userScore > 3) {
+        var scoreTag = '<span> EXCELLENT! You got <p>' + userScore + '</p> out of <p>' + questions.length + '</p></span>';
+        scoreText.innerHTML = scoreTag;
+    } else if (userScore > 1) {
+        var scoreTag = '<span> Nice...You got <p>' + userScore + '</p> out of <p>' + questions.length + '</p></span>';
+        scoreText.innerHTML = scoreTag;
+    } else {
+        var scoreTag = '<span> BummerYou got <p>' + userScore + '</p> out of <p>' + questions.length + '</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+}
+
